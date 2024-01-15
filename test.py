a98f7c4f1e02f9d6f7f68d4dd1b122d8276b74de
DimaG55-the-youtuber <dimgolv@gmail.com>
2024-01-15 18:00:28 -0500

    dew

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import requests
+import urllib.parse
 key = "AIzaSyCvnxM8LVlSqucFDecBG2Q1dFCZcrSm_Jw"
-book_search = requests.get("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=" + key)
-print(book_search.json)
+query = {"q": input("Book: "), "key": key}
+link = "https://www.googleapis.com/books/v1/volumes?"
+query = urllib.parse.urlencode(query)
+print(f"{link}{query}")
+book_search = requests.get(f"{link}{query}")
+# print(book_search.text)
